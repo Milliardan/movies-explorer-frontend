fd81416e343e71f287238187a9e69bbe29215462
Denis Kezhvatov <kezhvatovdenis@yandex.ru>
2023-09-15 22:18:03 +0200

    server url and deploy

--- src/utils/config.js
+++ src/utils/config.js
@@ -30,7 +30,7 @@ export const MOVIE_API = {
 };
 
 export const MAIN_API = {
-  BASE_URL: 'http://localhost:3003',
+  BASE_URL: 'https://api.movies.milliardan.nomoredomainsicu.ru',
   AUTH_HEADERS: {
     'Content-Type': 'application/json',
     Authorization: 'jwt token here',
